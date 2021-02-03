d0d82a859f30656741a9824c857a7ca397890973
mattetress <mattetress@me.com>
2021-02-03 10:20:17 -0600

    Initial Commit

---
+++ class_roster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  class_roster
+//
+//  Created by Matthew Etress on 2/3/21.
+//  Copyright © 2021 Matthew Etress. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
