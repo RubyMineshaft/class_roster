75e8463f84294d6a544e8ab41bfed923844fc590
mattetress <mattetress@me.com>
2021-02-03 15:30:23 -0600

    make class roster array public

--- class_roster/roster.h
+++ class_roster/roster.h
@@ -7,9 +7,9 @@ class Roster {
 private:
     int lastIndex = -1;
     const static int numStudents = 5;
-    Student* classRosterArray[numStudents];
     
 public:
+    Student* classRosterArray[numStudents];
     void parse(string row);
     void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram);
     void remove(string studentID);
