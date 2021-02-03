a9996203774317572d1e6208da861893968984aa
mattetress <mattetress@me.com>
2021-02-03 12:31:34 -0600

    declare roster class

--- class_roster/roster.h
+++ class_roster/roster.h
@@ -1,6 +1,24 @@
 #ifndef roster_h
 #define roster_h
 
-#include <stdio.h>
+#include "student.h"
+class Roster {
+    
+private:
+    int lastIndex = -1;
+    const static int numStudents = 5;
+    Student* classRosterArray[numStudents];
+    
+public:
+    void parse(string row);
+    void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram);
+    void remove(string studentID);
+    void printAll();
+    void printAverageDaysInCourse(string studentID);
+    void printInvalidEmails();
+    void printByDegreeProgram(DegreeProgram degreeProgram);
+    ~Roster();
+};
+
 
 #endif /* roster_h */
