0162993b2c7286d4448230a3ae74feabb7a0beeb
mattetress <mattetress@me.com>
2021-02-03 10:41:11 -0600

    create degree program enum

--- class_roster/degree.h
+++ class_roster/degree.h
@@ -11,4 +11,8 @@
 
 #include <stdio.h>
 
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+
+static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+
 #endif /* degree_h */
